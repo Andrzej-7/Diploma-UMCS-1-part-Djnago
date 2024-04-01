cc8466fddac280ce15112be2ecaf48fe7cc5953c
Andrzej-7 <84339062+Andrzej-7@users.noreply.github.com>
2024-04-01 19:20:28 +0200

    site footer html

--- exchange/models.py
+++ exchange/models.py
@@ -1,12 +1,10 @@
-#models.py 
+# models.py
 
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
-
 
 ETH = "ETH"
 XMR = "XMR"
@@ -16,11 +14,11 @@ Dogecoin = "Dogecoin"
 Bitcoin = "BTC"
 
 cryptoChoises = ((ETH, "eth"),
-                (XMR, "xmr"),
-                (DAI, "dai"),
-                (DASH, "dash"),
-                (Dogecoin, "dogecoin"),
-                (Bitcoin,"bitcoin"),)
+                 (XMR, "xmr"),
+                 (DAI, "dai"),
+                 (DASH, "dash"),
+                 (Dogecoin, "dogecoin"),
+                 (Bitcoin, "bitcoin"),)
 
 
 class Order(models.Model):
@@ -30,26 +28,21 @@ class Order(models.Model):
     recipient_wallet = models.CharField(max_length=255)
     crypto_to = models.CharField(max_length=255, choices=cryptoChoises)
     is_paid = models.BooleanField(default=False)
-    is_processed = models.BooleanField(default=False) 
+    is_processed = models.BooleanField(default=False)
     site_wallet = models.CharField(max_length=255, blank=True, null=True)
     you_get = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    
+
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
 class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=255)
-    
-
 
 
 class CryptoCurrency(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10)
 
-
-
     def __str__(self):
         return f'Order {self.id}'
-    
-
