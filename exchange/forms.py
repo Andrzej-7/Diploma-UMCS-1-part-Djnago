e3c65a0cc102cd4d6eae43f4d4433056c0d1be21
Andrzej-7 <84339062+Andrzej-7@users.noreply.github.com>
2024-04-28 22:37:06 +0200

    footer for all tempaltes

--- exchange/forms.py
+++ exchange/forms.py
@@ -1,12 +1,15 @@
 #forms.py
 
-from django import forms
+import django
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Order
 
 
-class OrderForm(forms.ModelForm):
+
+
+
+class OrderForm(django.forms.ModelForm):
 
 
     class Meta:
@@ -14,7 +17,7 @@ class OrderForm(forms.ModelForm):
         fields = ['email', 'crypto_from', 'amount', 'recipient_wallet', 'crypto_to', 'site_wallet']
 
         widgets = {
-            'site_wallet': forms.HiddenInput(),  # Приховування поля 'site_wallet'
+            'site_wallet': django.forms.HiddenInput(),  #Приховування поля 'site_wallet'
             
         }
 
@@ -26,10 +29,10 @@ class OrderForm(forms.ModelForm):
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
+    username = django.forms.CharField(widget=django.forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = django.forms.EmailField(widget=django.forms.EmailInput(attrs={'placeholder': 'E-mail'}))
+    password1 = django.forms.CharField(widget=django.forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = django.forms.CharField(widget=django.forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
     
 
     class Meta:
@@ -37,6 +40,6 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
         
-class loginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+class loginForm(django.forms.Form):
+    username = django.forms.CharField(widget=django.forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = django.forms.CharField(widget=django.forms.PasswordInput(attrs={'placeholder': 'Password'}))
